79aa17538d2cadd1f864900e89772c7d3deeb5b4
nasith <nasith@pepul.com>
2023-08-30 19:47:44 +0530

    add helmet tag

--- frontend/src/pages/support/index.jsx
+++ frontend/src/pages/support/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../App.css";
+import { Helmet } from "react-helmet";
+import logo from "../images/logo.png";
 import apple57x57 from "../images/apple-touch-icon-57x57.png";
 import apple72x72 from "../images/apple-touch-icon-72x72.png";
 import apple76x76 from "../images/apple-touch-icon-76x76.png";
@@ -13,43 +15,45 @@ import favicon from "../images/favicon.ico";
 export default function Support() {
     return (
         <div>
-            <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Shops Near Me</title>
+            <Helmet>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Shops Near Me</title>
 
-            <meta property="og:url" content="https://storesnearme.in/" />
-            <meta property="og:type" content="website" />
-            <meta property="og:site_name" content="Shops Near Me" />
-            <meta property="og:title" content="Shops Near Me" />
-            <meta property="og:description" content="Shop Near is a cutting-edge mobile application designed to enhance your shopping experience by providing a comprehensive and convenient way to discover and locate nearby stores, boutiques, and shopping centers. Whether you're searching for the latest fashion trends, electronics, home decor, or any other type of retail therapy, Shop Near is here to guide you." />
-            <meta property="og:image" content="https://storesnearme.in/images/banner.jpeg" />
+                <meta property="og:url" content="https://storesnearme.in/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="Shops Near Me" />
+                <meta property="og:title" content="Shops Near Me" />
+                <meta property="og:description" content="Shop Near is a cutting-edge mobile application designed to enhance your shopping experience by providing a comprehensive and convenient way to discover and locate nearby stores, boutiques, and shopping centers. Whether you're searching for the latest fashion trends, electronics, home decor, or any other type of retail therapy, Shop Near is here to guide you." />
+                <meta property="og:image" content="https://storesnearme.in/images/banner.jpeg" />
 
-            <meta property="og:locale" content="en_us" />
+                <meta property="og:locale" content="en_us" />
 
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:domain" value="https://storesnearme.in/" />
-            <meta name="twitter:url" value="https://storesnearme.in/" />
-            <meta name="twitter:title" content="Shops Near Me" />
-            <meta name="twitter:description" content="Shop Near is a cutting-edge mobile application designed to enhance your shopping experience by providing a comprehensive and convenient way to discover and locate nearby stores, boutiques, and shopping centers. Whether you're searching for the latest fashion trends, electronics, home decor, or any other type of retail therapy, Shop Near is here to guide you." />
-            <meta name="twitter:image" content="https://storesnearme.in/images/banner.jpeg" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:domain" value="https://storesnearme.in/" />
+                <meta name="twitter:url" value="https://storesnearme.in/" />
+                <meta name="twitter:title" content="Shops Near Me" />
+                <meta name="twitter:description" content="Shop Near is a cutting-edge mobile application designed to enhance your shopping experience by providing a comprehensive and convenient way to discover and locate nearby stores, boutiques, and shopping centers. Whether you're searching for the latest fashion trends, electronics, home decor, or any other type of retail therapy, Shop Near is here to guide you." />
+                <meta name="twitter:image" content="https://storesnearme.in/images/banner.jpeg" />
 
-            <link rel="shortcut icon" href={favicon} type="image/x-icon" />
-            <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
-            <link rel="apple-touch-icon" sizes="57x57" href={apple57x57} />
-            <link rel="apple-touch-icon" sizes="72x72" href={apple72x72} />
-            <link rel="apple-touch-icon" sizes="76x76" href={apple76x76} />
-            <link rel="apple-touch-icon" sizes="114x114" href={apple114x114} />
-            <link rel="apple-touch-icon" sizes="120x120" href={apple120x120} />
-            <link rel="apple-touch-icon" sizes="144x144" href={apple144x144} />
-            <link rel="apple-touch-icon" sizes="152x152" href={apple152x152} />
-            <link rel="apple-touch-icon" sizes="180x180" href={apple180x180} />
-            <meta name="theme-color" content="#000" />
-            <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+                <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+                <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
+                <link rel="apple-touch-icon" sizes="57x57" href={apple57x57} />
+                <link rel="apple-touch-icon" sizes="72x72" href={apple72x72} />
+                <link rel="apple-touch-icon" sizes="76x76" href={apple76x76} />
+                <link rel="apple-touch-icon" sizes="114x114" href={apple114x114} />
+                <link rel="apple-touch-icon" sizes="120x120" href={apple120x120} />
+                <link rel="apple-touch-icon" sizes="144x144" href={apple144x144} />
+                <link rel="apple-touch-icon" sizes="152x152" href={apple152x152} />
+                <link rel="apple-touch-icon" sizes="180x180" href={apple180x180} />
+                <meta name="theme-color" content="#000" />
+                <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+            </Helmet>
             <div class="support-theme">
                 <section class="boxed-up">
                     <header class="header-privacy">
-                        <img src="images/logo.png" alt="logo" />
+                        <img src={logo} alt="logo" />
                         <div>
                             <p>Shops</p>
                             <p>Near me</p>
